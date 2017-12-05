713c6cce682c6e51082e55393bc0a8f0ce01bdc4
Travis CI <hdf1986.6700@gmail.com>
2017-12-05 08:48:38 -0300

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :devices, only: [:show] do
+    member do
+      get :state
+      post :toggle
+    end
+  end
 end
